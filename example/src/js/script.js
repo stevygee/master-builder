6aa86b939465ae58af3fe55af8217033b0638550
Stefan Grassberger <mail@stefangrassberger.at>
2021-07-08 16:41:45 +0200

    Add ES6 example

--- example/src/js/script.js
+++ example/src/js/script.js
@@ -1,5 +1,10 @@
 let number = Math.min(2,3);
 console.log('test', number);
 
+/* ES6 */
+var newOne = () => {
+  console.log("Hello World..!");
+}
+
 /* JSX */
 const element = <h1>Hello, world!</h1>;
