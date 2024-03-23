3603a71cb9bd0161580277446062ff38845cae1f
Stefan Grassberger <m@stevygee.com>
2024-03-23 12:23:39 +0100

    Avoid spamming OS with notifications

--- src/notifier.js
+++ src/notifier.js
@@ -7,15 +7,15 @@ export let envPlugin = {
 	setup( build ) {
 		build.onEnd( (res) => {
 			if ( res.errors.length > 0 ) {
-				res.errors.forEach( (e) => {
-					const title = 'master-builder: Error during esbuild 😓';
-					const message = e.text;
+				const e = res.errors[0]; // don't spam OS, display first error only
 
-					notifier.notify( {
-						'app-name': 'my app',
-						title,
-						message,
-					} );
+				const title = 'master-builder: Error during esbuild 😓';
+				const message = e.text;
+
+				notifier.notify( {
+					'app-name': 'my app',
+					title,
+					message,
 				} );
 				previouslyFailed = true;
 			} else if( previouslyFailed ) {
