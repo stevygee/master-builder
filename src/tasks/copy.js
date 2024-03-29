96f43f7aebf0bf93f08a6914bbec2c3c5103973f
Stefan Grassberger <m@stevygee.com>
2024-03-29 12:47:03 +0100

    Watch copy, selective copy

--- src/tasks/copy.js
+++ src/tasks/copy.js
@@ -2,10 +2,14 @@ import { config } from '../config.js';
 
 import log from 'fancy-log';
 import colors from 'ansi-colors';
+
 import cpy from 'cpy';
 import path from 'path';
+import { glob } from 'glob';
+import { watchFile } from 'node:fs';
+import { stat } from 'fs/promises';
 
-export async function copyFiles( files, destinationPathPrefix = '' ) {
+export async function copyFiles( files, destinationPathPrefix = '', includePaths = [] ) {
 	if ( typeof files === 'undefined' ) {
 		// skip task if missing in config
 		return;
@@ -16,6 +20,8 @@ export async function copyFiles( files, destinationPathPrefix = '' ) {
 		let destinationFolder = path.resolve( destinationPathPrefix + v.destinationFolder );
 
 		await cpy( v.sourceFiles, destinationFolder, {
+			// copy only files in includePath, if set
+			filter: file => includePaths.length < 1 ? true : includePaths.includes( file.path ),
 			cwd: v.sourceFolder,
 			parents: true,
 		} ).then( (res) => {
@@ -28,6 +34,44 @@ export async function copyFiles( files, destinationPathPrefix = '' ) {
 	} ) );
 }
 
+async function copyFile( f, files ) {
+	const fileExists = await stat( f )
+		.then(() => true)
+		.catch(() => false);
+
+	// File was deleted, there is nothing to copy
+	if ( ! fileExists ) {
+		return;
+	}
+
+	// Get absolute path
+	let includePaths = [];
+	includePaths.push( path.resolve( f ) );
+
+	// Copy only changed file
+	await copyFiles( files, '', includePaths );
+}
+
 export default async function perform() {
+	if ( typeof config.copy === 'undefined' ) {
+		// skip task if missing in config
+		return;
+	}
+
 	await copyFiles( config.copy );
+
+	// Watch files
+	for ( let v2 of config.copy ) {
+		for ( let v of v2.sourceFiles ) {
+			const filePath = v2.sourceFolder.endsWith( '/' ) ? v2.sourceFolder + v : v2.sourceFolder + '/' + v;
+			const fileNames = await glob( filePath );
+
+			for ( let f of fileNames ) {
+				watchFile( f, (curr, prev) => {
+					log( `File changed: ${colors.magenta( f )}` );
+					copyFile( f, config.copy );
+				} );
+			}
+		}
+	}
 }
